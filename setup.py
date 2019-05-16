4117bad3493920b91de68a633bb0a513e9fc5341
Laurie Opperman <laurie_opperman@hotmail.com>
2019-05-16 14:56:35 +1000

    Refactored; added 'states' keyword

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@
 import pathlib
 import setuptools
 
-_parent = pathlib.Path(__file__).parent
-_long_description = (_parent / "README.md").read_text()
-_version = (_parent / "VERSION").read_text().strip()
-_classifiers = [
+parent = pathlib.Path(__file__).parent
+long_description = (parent / "README.md").read_text()
+version = (parent / "VERSION").read_text().strip()
+classifiers = [
     "Environment :: Console",
     "Intended Audience :: Developers",
     "Programming Language :: Python :: 3 :: Only",
@@ -18,28 +18,29 @@ _classifiers = [
     "Operating System :: Microsoft :: Windows",
     "Operating System :: MacOS",
     "License :: OSI Approved :: MIT License"]
+extras_require = {
+        "dev": [
+            "pytest",
+            "pytest-cov",
+            "moto",
+            "sphinx",
+            "sphinx_rtd_theme"]}
 
 setuptools.setup(
     name="sfini",
-    version=_version,
+    version=version,
     license="MIT",
     author="Laurie",
     author_email="laurie@sitesee.com.au",
     maintainer="Laurie",
     maintainer_email="laurie@sitesee.com.au",
     description="Create, run and manage AWS Step Functions easily",
-    long_description=_long_description,
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://gitlab.com/Epic_Wink/aws-sfn-service",
-    classifiers=_classifiers,
-    keywords="aws sfn service step functions",
+    classifiers=classifiers,
+    keywords="aws sfn service step functions states",
     packages=setuptools.find_packages(exclude=["tests/*"]),
     python_requires="~=3.5",
     install_requires=["boto3"],
-    extras_require={
-        "dev": [
-            "pytest",
-            "pytest-cov",
-            "moto",
-            "sphinx",
-            "sphinx_rtd_theme"]})
+    extras_require=extras_require)
