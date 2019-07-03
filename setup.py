f407d308d814cc8602a0f70eda75b5ea4f5c4051
Laurie Opperman <laurie_opperman@hotmail.com>
2019-07-03 22:33:16 +1000

    Remove intermediary variable creation

--- setup.py
+++ setup.py
@@ -9,28 +9,6 @@ import setuptools
 parent = pathlib.Path(__file__).parent
 long_description = (parent / "README.md").read_text()
 version = (parent / "VERSION").read_text().strip()
-classifiers = [
-    "Environment :: Console",
-    "Intended Audience :: Developers",
-    "Programming Language :: Python :: 3 :: Only",
-    "Natural Language :: English",
-    "Operating System :: POSIX :: Linux",
-    "Operating System :: Microsoft :: Windows",
-    "Operating System :: MacOS",
-    "License :: OSI Approved :: MIT License"]
-extras_require = {
-    "dev": [
-        "pytest",
-        "pytest-cov",
-        "pytest-timeout",
-        "moto",
-        "sphinx",
-        "sphinx_rtd_theme"]}
-project_urls = {
-    "Documentation": "https://sfini.readthedocs.io/en/latest/",
-    "Source": "https://github.com/EpicWink/sfini",
-    "Bugs": "https://github.com/EpicWink/sfini/issues"}
-packages = setuptools.find_packages(where="src")
 
 setuptools.setup(
     name="sfini",
@@ -44,11 +22,29 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/EpicWink/sfini",
-    classifiers=classifiers,
+    classifiers=[
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3 :: Only",
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS",
+        "License :: OSI Approved :: MIT License"],
     keywords="aws sfn service step functions states",
-    packages=packages,
+    packages=setuptools.find_packages(where="src"),
     package_dir={"": "src"},
     python_requires="~=3.6",
     install_requires=["boto3"],
-    extras_require=extras_require,
-    project_urls=project_urls)
+    extras_require={
+        "dev": [
+            "pytest",
+            "pytest-cov",
+            "pytest-timeout",
+            "moto",
+            "sphinx",
+            "sphinx_rtd_theme"]},
+    project_urls={
+        "Documentation": "https://sfini.readthedocs.io/en/latest/",
+        "Source": "https://github.com/EpicWink/sfini",
+        "Bugs": "https://github.com/EpicWink/sfini/issues"})
